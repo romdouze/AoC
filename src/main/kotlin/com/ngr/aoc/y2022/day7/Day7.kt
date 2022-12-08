f2c4af0e80a1cdbf6ba5191961c78154a8c0576b
Romain Mouchel-Vallon <r.mouchel-vallon@dental-monitoring.com>
2022-12-08 11:10:34 +0100

    clean day 7

--- src/main/kotlin/com/ngr/aoc/y2022/day7/Day7.kt
+++ src/main/kotlin/com/ngr/aoc/y2022/day7/Day7.kt
@@ -10,21 +10,20 @@ class Day7 : Day<ShellEntry, Int, Int>() {
     }
 
     private val root = Dir("/", null)
-    private val dirs = mutableSetOf<Dir>()
+    private var currentDir = root
+    private val dirs = mutableSetOf(root)
 
     override fun handleLine(lines: MutableList<ShellEntry>, line: String) {
-        lines.add(ShellEntry.fromString(line))
+        buildTree(ShellEntry.fromString(line))
     }
 
     override fun part1(lines: List<ShellEntry>): Int {
-        buildTree(lines)
         return dirs
             .filter { it.size <= 100000 }
             .sumOf { it.size }
     }
 
     override fun part2(lines: List<ShellEntry>): Int {
-        buildTree(lines)
         val missingSpace = REQUIRED_SPACE - (TOTAL_SPACE - root.size)
         return dirs
             .filter { it.size >= missingSpace }
@@ -32,24 +31,25 @@ class Day7 : Day<ShellEntry, Int, Int>() {
             .size
     }
 
-    private fun buildTree(entries: List<ShellEntry>) {
-        var currentDir = root
-        entries.drop(1) // Yes, we drop the first instruction which is $ cd / and is annoying to account for.
-            .forEach { entry ->
-                when (entry) {
-                    is DirEntry,
-                    is FileEntry ->
-                        File.fromShellEntry(entry, currentDir).also {
-                            currentDir.files[it.name] = it
-                            if (it is Dir) dirs.add(it)
-                        }
+    private fun buildTree(entry: ShellEntry) {
+        when (entry) {
+            is DirEntry,
+            is FileEntry ->
+                File.fromShellEntry(entry, currentDir).also {
+                    currentDir.files[it.name] = it
+                    if (it is Dir) dirs.add(it)
+                }
 
-                    is CommandEntry ->
-                        if (entry.command == Command.CD) {
-                            currentDir = currentDir.files[entry.param] as? Dir
+            is CommandEntry ->
+                if (entry.command == Command.CD) {
+                    currentDir =
+                        if (entry.param == "/") {
+                            root
+                        } else {
+                            currentDir.files[entry.param] as? Dir
                                 ?: throw IllegalArgumentException("Cannot cd to a file that is not a dir: ${entry.param}")
                         }
                 }
-            }
+        }
     }
 }
