31feaffcb5e3c957dcfe67607b05206f3d79e033
rom.douze@gmail.com <rom.douze@gmail.com>
2022-12-20 23:59:03 +0100

    day 16

--- src/main/kotlin/com/ngr/aoc/y2022/day16/Day16.kt
+++ src/main/kotlin/com/ngr/aoc/y2022/day16/Day16.kt
@@ -7,8 +7,6 @@ class Day16 : Day<String, Int, Int>() {
     private companion object {
         private const val START = "AA"
         private const val MAX_TIME = 30
-        private const val MOVE_TIME = 1
-        private const val OPEN_TIME = 1
     }
 
     private val initialNetwork: MutableMap<String, Valve> = mutableMapOf()
@@ -19,6 +17,7 @@ class Day16 : Day<String, Int, Int>() {
     }
 
     override fun part1(lines: List<String>): Int {
+        val maxTime = MAX_TIME
         val network: Network = initialNetwork.entries.associate { it.key to Node(it.value) }
             .plus(
                 initialNetwork.entries
@@ -28,104 +27,37 @@ class Day16 : Day<String, Int, Int>() {
 
         val allPaths = mutableListOf<Path>()
         network.generatePaths(
-            MAX_TIME,
-            Path(0, listOf(START)),
-            network.values.filter { it.open }.map { it.name },
+            maxTime,
+            emptyPath(START),
+            network.allOpenable.map { it.name },
             allPaths
         )
 
-        val bestPath = allPaths.maxBy { network.released(it, MAX_TIME) }
+        val bestPath = allPaths.maxBy { network.released(it, maxTime) }
 
-//        var currentNode = network[START]
-//            ?.apply {
-//                elapsed = 0
-//                released = 0
-//                path = Path(0, listOf(this.name))
-//            }
-//
-//        val toVisit = network.allOpenable.toMutableList()
-//
-//        while (currentNode != null && network.values.any { it.elapsed < MAX_TIME }) {
-//
-//            val remainingTime = MAX_TIME - currentNode.elapsed
-//
-//            println("time: $remainingTime, currentNode: $currentNode")
-//
-//            toVisit
-//                .filter { currentNode!!.name != it.name }
-//                .onEach { nextToVisit ->
-//                    val path = network.pathTo(currentNode!!.name, nextToVisit.name)
-//                    val tentativeReleased =
-//                        currentNode!!.released + nextToVisit.valve.rate * (remainingTime - path.elapsed)
-//                    val tentativeElapsed = currentNode!!.elapsed + path.elapsed
-//                    if (tentativeElapsed <= MAX_TIME && tentativeReleased > nextToVisit.released) {
-//                        nextToVisit.released = tentativeReleased
-//                        nextToVisit.elapsed = tentativeElapsed
-//                        nextToVisit.path = currentNode!!.path + path
-//                    }
-//                }
-//
-//            toVisit.remove(currentNode)
-//            currentNode = toVisit
-//                .filter { it.elapsed < MAX_TIME }
-//                .maxByOrNull { it.released }
-//        }
-
-//        val toVisit = network.allOpenable.toMutableList()
-//        var bestPath = Path(0, listOf(START))
-//        var finished = false
-//
-//        while (toVisit.isNotEmpty() && !finished) {
-//            val next = toVisit
-//                .filter { next ->
-//                    bestPath.elapsed + network.pathTo(bestPath.path.last(), next.name).elapsed <= MAX_TIME
-//                }
-//                .maxByOrNull { next ->
-//                    (MAX_TIME - network.pathTo(bestPath.path.last(), next.name).elapsed) * next.valve.rate
-//                }
-//
-//            if (next != null) {
-//                bestPath += network.pathTo(bestPath.path.last(), next.name)
-//                toVisit.remove(next)
-//            } else finished = true
-//        }
-
-        return network.released(bestPath, MAX_TIME)
-        //        return network.values.maxOf { it.released }
+        return network.released(bestPath, maxTime)
     }
 
+    // WAY TOO SLOW !
     override fun part2(lines: List<String>): Int {
-        TODO("Not yet implemented")
-    }
+        val maxTime = MAX_TIME - 4
+        val network: Network = initialNetwork.entries.associate { it.key to Node(it.value) }
+            .plus(
+                initialNetwork.entries
+                    .filter { it.value.rate > 0 }
+                    .associate { it.key.asOpen() to Node(it.value, open = true) }
+            )
 
-    private fun Network.generatePaths(
-        maxTime: Int,
-        currentPath: Path,
-        availableNodes: List<String>,
-        allPaths: MutableList<Path>,
-    ) {
-        availableNodes
-            .map { currentPath + pathTo(currentPath.end, it) }
-            .forEach { next ->
-                val currentBest = allPaths.maxOfOrNull { released(it, MAX_TIME) } ?: Int.MIN_VALUE
-                val available = availableNodes.minus(next.end)
-                    .filter { (next + pathTo(next.end, it)).elapsed <= MAX_TIME }
-                if (available.isNotEmpty()) {
-                    if (bestPotential(next, available, maxTime) > currentBest) {
-                        generatePaths(maxTime, next, available, allPaths)
-                    }
-                } else {
-                    if (released(next, maxTime) > currentBest) {
-                        println("adding path: $next")
-                        allPaths.add(next)
-                    }
-                }
-            }
-    }
+        val allPaths = mutableListOf<Pair<Path, Path>>()
+        network.generateMultiPath(
+            maxTime,
+            emptyPath(START) to emptyPath(START),
+            network.allOpenable.map { it.name } to network.allOpenable.map { it.name },
+            allPaths
+        )
 
-    private fun Network.bestPotential(partialPath: Path, availableNodes: List<String>, maxTime: Int): Int {
-        val minTime = if (availableNodes.isEmpty()) 0 else availableNodes.minOf { pathTo(partialPath.end, it).elapsed }
-        return released(partialPath, maxTime) + availableNodes.mapNotNull { this[it] }
-            .sumOf { it.valve.rate * (maxTime - partialPath.elapsed - minTime) }
+        val bestPath = allPaths.maxBy { it.toList().sumOf { network.released(it, maxTime) } }
+
+        return bestPath.toList().sumOf { network.released(it, maxTime) }
     }
 }
--- src/main/kotlin/com/ngr/aoc/y2022/day16/Network.kt
+++ src/main/kotlin/com/ngr/aoc/y2022/day16/Network.kt
@@ -7,6 +7,8 @@ val Path.path get() = second
 val Path.start get() = path.first()
 val Path.end get() = path.last()
 
+fun emptyPath(start: String) = Path(0, listOf(start))
+
 operator fun Path.plus(other: Path) =
     Path(elapsed + other.elapsed, path.dropLast(1) + other.path)
 
@@ -36,8 +38,12 @@ fun Network.released(path: Path, remainingTimeAtStart: Int) =
         }
 
 fun Network.pathTo(start: String, end: String) =
-    pathCache.computeIfAbsent(start to end) {
-        computePath(start, end)
+    if (end == NONE) {
+        emptyPath(start)
+    } else {
+        pathCache.computeIfAbsent(start to end) {
+            computePath(start, end)
+        }
     }
 
 private fun Network.computePath(start: String, end: String): Path {
@@ -64,4 +70,94 @@ private fun Network.computePath(start: String, end: String): Path {
     }
 
     return flood[end]!!
+}
+
+private const val NONE = "NONE"
+
+fun Network.generateMultiPath(
+    maxTime: Int,
+    currentMultiPath: Pair<Path, Path>,
+    availableNodes: Pair<List<String>, List<String>>,
+    allPaths: MutableList<Pair<Path, Path>>
+) {
+    availableNodes.first
+        .map {
+            it to
+                    currentMultiPath.first + pathTo(currentMultiPath.first.end, it)
+        }
+        .ifEmpty { listOf(NONE to currentMultiPath.first) }
+        .forEach { first ->
+            val (firstPick, firstNext) = first
+            availableNodes.second
+                .minus(firstPick)
+                .map {
+                    it to
+                            currentMultiPath.second + pathTo(currentMultiPath.second.end, it)
+                }
+                .ifEmpty { listOf(NONE to currentMultiPath.second) }
+                .forEach { second ->
+                    val (secondPick, secondNext) = second
+                    val currentBest =
+                        allPaths.maxOfOrNull { it.toList().sumOf { released(it, maxTime) } } ?: Int.MIN_VALUE
+                    val available =
+                        availableNodes.first.minus(listOf(firstPick, secondPick).toSet())
+                            .filter {
+                                (firstNext + pathTo(firstPick, it)).elapsed <= maxTime
+                            } to
+                                availableNodes.second.minus(listOf(firstPick, secondPick).toSet())
+                                    .filter {
+                                        (secondNext + pathTo(secondPick, it)).elapsed <= maxTime
+                                    }
+                    if (available.toList().any { it.isNotEmpty() }) {
+                        if (listOf(firstNext to available.first, secondNext to available.second)
+                                .sumOf { bestPotential(it.first, it.second, maxTime) } > currentBest
+                        ) {
+                            generateMultiPath(maxTime, firstNext to secondNext, available, allPaths)
+                        }
+                    } else {
+                        if (listOf(firstNext, secondNext).sumOf { released(it, maxTime) } > currentBest) {
+                            println(
+                                "\nadding multiPath:\n\t$firstNext\n\t$secondNext\n=>${
+                                    listOf(
+                                        firstNext,
+                                        secondNext
+                                    ).sumOf { released(it, maxTime) }
+                                }"
+                            )
+                            allPaths.add(firstNext to secondNext)
+                        }
+                    }
+                }
+        }
+}
+
+fun Network.generatePaths(
+    maxTime: Int,
+    currentPath: Path,
+    availableNodes: List<String>,
+    allPaths: MutableList<Path>,
+) {
+    availableNodes
+        .map { currentPath + pathTo(currentPath.end, it) }
+        .forEach { next ->
+            val currentBest = allPaths.maxOfOrNull { released(it, maxTime) } ?: Int.MIN_VALUE
+            val available = availableNodes.minus(next.end)
+                .filter { (next + pathTo(next.end, it)).elapsed <= maxTime }
+            if (available.isNotEmpty()) {
+                if (bestPotential(next, available, maxTime) > currentBest) {
+                    generatePaths(maxTime, next, available, allPaths)
+                }
+            } else {
+                if (released(next, maxTime) > currentBest) {
+//                    println("adding path: $next")
+                    allPaths.add(next)
+                }
+            }
+        }
+}
+
+fun Network.bestPotential(partialPath: Path, availableNodes: List<String>, maxTime: Int): Int {
+    val minTime = if (availableNodes.isEmpty()) 0 else availableNodes.minOf { pathTo(partialPath.end, it).elapsed }
+    return released(partialPath, maxTime) + availableNodes.mapNotNull { this[it] }
+        .sumOf { it.valve.rate * (maxTime - partialPath.elapsed - minTime) }
 }
