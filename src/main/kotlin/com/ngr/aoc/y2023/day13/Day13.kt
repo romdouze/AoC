4d9f84df446f1b4fc584fc4a4c690ad8add6fa63
Romain Mouchel-Vallon <r.mouchel-vallon@dental-monitoring.com>
2023-12-13 17:23:52 +0100

    init

---
+++ src/main/kotlin/com/ngr/aoc/y2023/day13/Day13.kt
@@ -0,0 +1,17 @@
+package com.ngr.aoc.y2023.day13
+
+import com.ngr.aoc.Day
+
+class Day13 : Day<List<String>, Int, Int>() {
+    override fun handleLine(lines: MutableList<List<String>>, line: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun part1(lines: List<List<String>>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(lines: List<List<String>>): Int {
+        TODO("Not yet implemented")
+    }
+}
