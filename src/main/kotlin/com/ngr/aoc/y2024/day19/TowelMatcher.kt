524f6b4201adddacb07d0806b597fce3af29a7d9
RMV <rom.douze@gmail.com>
2025-01-11 17:34:48 +0100

    wip

--- src/main/kotlin/com/ngr/aoc/y2024/day19/TowelMatcher.kt
+++ src/main/kotlin/com/ngr/aoc/y2024/day19/TowelMatcher.kt
@@ -4,24 +4,35 @@ class TowelMatcher(
     private val towels: List<String>,
 ) {
     private companion object {
-        private val patternMatches = mutableMapOf<String, MutableList<String>>()
+        private val patternMatches = mutableMapOf<String, MutableSet<List<String>>>()
+
+        private fun addToCache(pattern: String, match: List<String>) {
+            patternMatches.compute(pattern) { _, it ->
+                it?.also {
+                    it.add(match)
+                } ?: mutableSetOf(match)
+            }
+        }
     }
 
-    fun match(pattern: String) =
-        findMatch(pattern, emptyList())
+    fun match(pattern: String): List<List<String>> {
+        val matches = mutableListOf<List<String>>()
+        findMatch(pattern, 0, emptyList(), matches)
+        return matches
+    }
 
-    private fun findMatch(pattern: String, match: List<String>): List<String> {
-        if (pattern.isEmpty()) {
-            return match
-        }
-        return patternMatches[pattern] ?: towels.filter {
-            pattern.startsWith(it)
-        }.flatMap {
-            findMatch(pattern.substring(it.length), match + it)
-                .also {
-                    patternMatches.computeIfAbsent(pattern) { mutableListOf() }
-                        .addAll(it)
-                }
+    private fun findMatch(pattern: String, index: Int, currentMatch: List<String>, matches: MutableList<List<String>>) {
+        addToCache(pattern.take(index), currentMatch)
+
+        val partialPattern = pattern.drop(index)
+        if (partialPattern.isEmpty()) {
+            matches.add(currentMatch)
+        } else {
+            towels.filter {
+                partialPattern.startsWith(it)
+            }.forEach { match ->
+                findMatch(pattern, index + match.length, currentMatch + match, matches)
+            }
         }
     }
 }
