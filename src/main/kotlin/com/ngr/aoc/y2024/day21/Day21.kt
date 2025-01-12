83ea64c926b22024fbbe9256692e0d137860be68
RMV <rom.douze@gmail.com>
2025-01-12 17:26:16 +0100

    init

---
+++ src/main/kotlin/com/ngr/aoc/y2024/day21/Day21.kt
@@ -0,0 +1,17 @@
+package com.ngr.aoc.y2024.day21
+
+import com.ngr.aoc.Day
+
+class Day21 : Day<String, Int, Int>() {
+    override fun handleLine(lines: MutableList<String>, line: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun part1(lines: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(lines: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+}
