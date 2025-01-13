1881922ebdd6e02d45cbe695544b6d120a176391
RMV <rom.douze@gmail.com>
2025-01-13 22:06:04 +0100

    keypad operated

--- src/main/kotlin/com/ngr/aoc/y2024/day21/Day21.kt
+++ src/main/kotlin/com/ngr/aoc/y2024/day21/Day21.kt
@@ -1,15 +1,24 @@
 package com.ngr.aoc.y2024.day21
 
 import com.ngr.aoc.Day
+import com.ngr.aoc.y2024.day21.KeypadType.DIRECTIONAL
+import com.ngr.aoc.y2024.day21.KeypadType.NUMERIC
 
 class Day21 : Day<String, Int, Int>() {
     override fun handleLine(lines: MutableList<String>, line: String) {
         lines.add(line)
     }
 
-    override fun part1(lines: List<String>): Int {
-        TODO("Not yet implemented")
-    }
+    override fun part1(lines: List<String>) =
+        lines.sumOf { code ->
+            KeypadOperator(DIRECTIONAL).inputForCode(
+                KeypadOperator(DIRECTIONAL).inputForCode(
+                    KeypadOperator(NUMERIC).inputForCode(
+                        code.toKeys()
+                    ).toKeys()
+                ).toKeys()
+            ).size * code.dropLast(1).toInt()
+        }
 
     override fun part2(lines: List<String>): Int {
         TODO("Not yet implemented")
---
+++ src/main/kotlin/com/ngr/aoc/y2024/day21/KeypadOperator.kt
@@ -0,0 +1,132 @@
+package com.ngr.aoc.y2024.day21
+
+import com.ngr.aoc.y2024.day21.Key.A
+import java.awt.Point
+
+class KeypadOperator(
+    keypadType: KeypadType,
+) {
+    private val keypad = Keypad(keypadType)
+    private var key = A
+
+    fun inputForCode(code: List<Key>) =
+        code.flatMap { nextKey ->
+            (keypad.pathTo(key, nextKey) + Move.A)
+                .also { key = nextKey }
+        }
+}
+
+class Keypad(
+    private val type: KeypadType
+) {
+
+    fun pathTo(from: Key, to: Key) =
+        type.pathCache.computeIfAbsent(from) { mutableMapOf() }
+            .computeIfAbsent(to) {
+                computePathTo(from, to)
+            }
+
+    private fun computePathTo(from: Key, to: Key): List<Move> {
+        val fromPos = posOf(from)
+        val toPos = posOf(to)
+        val visited = mutableMapOf(fromPos to emptyList<Move>())
+        val toVisit = ArrayDeque(listOf(fromPos))
+
+        while (toVisit.isNotEmpty() && !visited.containsKey(toPos)) {
+            val currentPos = toVisit.removeFirst()
+
+            Move.moves.forEach { move ->
+                val newPos = currentPos + move
+                if (type.poses.contains(newPos) &&
+                    !visited.containsKey(newPos) &&
+                    !toVisit.contains(newPos)
+                ) {
+                    toVisit.add(newPos)
+                    visited[newPos] = visited[currentPos]!! + move
+                }
+            }
+        }
+
+        return visited[toPos]!!
+    }
+
+    private fun posOf(key: Key): Point = type.keyPositions[key]
+        ?: error("No key $key for keypad $type")
+}
+
+enum class KeypadType(
+    val keyPositions: Map<Key, Point>,
+    val keys: Set<Key> = keyPositions.keys,
+    val poses: Set<Point> = keyPositions.values.toSet()
+) {
+    NUMERIC(
+        mapOf(
+            Key.A to Point(2, 3),
+            Key.`0` to Point(1, 3),
+            Key.`1` to Point(0, 2),
+            Key.`2` to Point(1, 2),
+            Key.`3` to Point(2, 2),
+            Key.`4` to Point(0, 1),
+            Key.`5` to Point(1, 1),
+            Key.`6` to Point(2, 1),
+            Key.`7` to Point(0, 0),
+            Key.`8` to Point(0, 1),
+            Key.`9` to Point(0, 2),
+        )
+    ),
+    DIRECTIONAL(
+        mapOf(
+            Key.A to Point(2, 0),
+            Key.`>` to Point(2, 1),
+            Key.v to Point(1, 1),
+            Key.`<` to Point(0, 1),
+            Key.`^` to Point(1, 0),
+        )
+    );
+
+    val pathCache = mutableMapOf<Key, MutableMap<Key, List<Move>>>()
+}
+
+enum class Key {
+    `>`,
+    v,
+    `<`,
+    `^`,
+    A,
+    `0`,
+    `1`,
+    `2`,
+    `3`,
+    `4`,
+    `5`,
+    `6`,
+    `7`,
+    `8`,
+    `9`;
+
+    companion object {
+        fun fromString(key: String) =
+            entries.first { it.name == key }
+    }
+}
+
+enum class Move(val dx: Int, val dy: Int, val key: Key) {
+    E(1, 0, Key.`>`),
+    S(0, 1, Key.v),
+    W(-1, 0, Key.`<`),
+    N(0, -1, Key.`^`),
+    A(0, 0, Key.A);
+
+    companion object {
+        val moves = entries.minus(A)
+    }
+}
+
+operator fun Point.plus(move: Move) =
+    Point(x + move.dx, y + move.dy)
+
+fun List<Move>.toKeys() =
+    map { it.key }
+
+fun String.toKeys() =
+    map { Key.fromString(it.toString()) }
